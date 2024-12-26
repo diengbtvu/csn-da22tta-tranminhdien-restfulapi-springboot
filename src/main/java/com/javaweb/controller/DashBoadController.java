4636086c66ec45bbd94a8f015d42c1e98523bb4c
diengbtvu <110122050@st.tvu.edu.vn>
2024-12-26 13:09:51 -0500

    update ui dashboard

---
+++ src/main/java/com/javaweb/controller/DashBoadController.java
@@ -0,0 +1,13 @@
+package com.javaweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashBoadController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
