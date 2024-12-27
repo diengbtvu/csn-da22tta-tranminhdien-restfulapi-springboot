df34f9dca3eadc44e4a9738634fbf1494f274c38
diengbtvu <110122050@st.tvu.edu.vn>
2024-12-27 03:22:22 -0500

    done fe ui

--- src/main/java/com/javaweb/controller/DashBoadController.java
+++ src/main/java/com/javaweb/controller/DashBoadController.java
@@ -10,4 +10,21 @@ public class DashBoadController {
     public String index() {
         return "index";
     }
+    @GetMapping("/quanlycanho")
+    public String indexqlch() {
+        return "index2_qlch";
+    }
+    @GetMapping("/quanlykhachhang")
+    public String indexqlkh() {
+        return "index4_qlkh";
+    }
+    @GetMapping("/quanlythietbi")
+    public String indexqltb() {
+        return "index5_qltb";
+    }
+    @GetMapping("/quanlyhopdong")
+    public String indexqlhd() {
+        return "index3_qlhd";
+    }
+
 }
