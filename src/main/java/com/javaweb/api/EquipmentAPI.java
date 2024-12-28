f04bddbbb6cce5ddcd733204174c733ac16d534e
diengbtvu <110122050@st.tvu.edu.vn>
2024-12-28 08:15:00 -0500

    update report

---
+++ src/main/java/com/javaweb/api/EquipmentAPI.java
@@ -0,0 +1,4 @@
+package com.javaweb.api;
+
+public class EquipmentAPI {
+}
---
+++ src/main/java/com/javaweb/dto/ApartmentEquipmentBuilder.java
@@ -0,0 +1,21 @@
+package com.javaweb.dto;
+
+import java.util.Date;
+
+public class ApartmentEquipmentDTO {
+    private Long apartmentId;
+    private String apartmentName;
+    private Double minRate;
+    private Double floorArea;
+    private Integer numberOfBedrooms;
+    private Integer numberOfBathrooms;
+    private Boolean rented;
+    private Date moveInDate;
+    private Date moveOutDate;
+    private Long equipmentId;
+    private String equipmentType;
+    private String equipmentStatus;
+    private Double brokenFee;
+
+
+}
