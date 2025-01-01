3faf552a3e582598867076f410133f9b44a032a4
diengbtvu <110122050@st.tvu.edu.vn>
2025-01-01 06:21:26 -0500

    cm

--- src/main/java/com/javaweb/controller/ContractController.java
+++ src/main/java/com/javaweb/controller/ContractController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/quanlyhopdong")
+@RequestMapping("/hopdong")
 public class ContractController {
 
     @Autowired
