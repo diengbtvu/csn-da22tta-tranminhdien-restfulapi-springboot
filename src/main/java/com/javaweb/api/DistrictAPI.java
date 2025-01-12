5785cbae13bcdf0a3182b8c0f924d8afef75c0b2
diengbtvu <110122050@st.tvu.edu.vn>
2025-01-12 11:16:43 -0500

    add remote db

---
+++ src/main/java/com/javaweb/api/DistrictAPI.java
@@ -0,0 +1,21 @@
+package com.javaweb.api;
+
+import com.javaweb.repository.DistrictRepository;
+import com.javaweb.repository.entity.DistrictEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/districts")
+public class DistrictAPI {
+    @Autowired
+    private DistrictRepository districtRepository;
+
+        public ResponseEntity<Void> createDistrict(@RequestBody DistrictEntity districtEntity) {
+        return districtRepository.save(districtEntity) != null ? ResponseEntity.ok().build() : ResponseEntity.badRequest().build();
+    }
+
+}
