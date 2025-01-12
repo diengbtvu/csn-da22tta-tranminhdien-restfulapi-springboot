d330400deba7ebaaadfcfa3e529352414207786a
diengbtvu <110122050@st.tvu.edu.vn>
2025-01-12 11:30:00 -0500

    done api dt

--- src/main/java/com/javaweb/api/DistrictAPI.java
+++ src/main/java/com/javaweb/api/DistrictAPI.java
@@ -4,6 +4,7 @@ import com.javaweb.repository.DistrictRepository;
 import com.javaweb.repository.entity.DistrictEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DistrictAPI {
     @Autowired
     private DistrictRepository districtRepository;
-
+        @PostMapping
         public ResponseEntity<Void> createDistrict(@RequestBody DistrictEntity districtEntity) {
         return districtRepository.save(districtEntity) != null ? ResponseEntity.ok().build() : ResponseEntity.badRequest().build();
     }
